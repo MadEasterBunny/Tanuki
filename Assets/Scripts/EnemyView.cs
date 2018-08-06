f69aeb31f60898e7f8502d3c94034c7ff9ecf061
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-06 20:57:10 +0900

    オリジナルゲーム「たぬき」です。

---
+++ Assets/Scripts/EnemyView.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyView : MonoBehaviour
+{
+    public Light spotLight;
+    public float viewDistance;
+    public LayerMask viewMask;
+
+    private Color originalColor;
+    private float viewAngle;
+    Transform player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        viewAngle = spotLight.spotAngle;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        originalColor = spotLight.color;
+	}
+
+    bool CanSeePlayer()
+    {
+        if(Vector3.Distance(transform.position, player.position) < viewDistance)
+        {
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            float angleBetweenEnemyAndPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+            if (angleBetweenEnemyAndPlayer < viewAngle / 2f)
+            {
+                if (!Physics.Linecast(transform.position, player.position, viewMask))
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(CanSeePlayer())
+        {
+            spotLight.color = Color.red;
+        }
+        else
+        {
+            spotLight.color = originalColor;
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
+    }
+}
