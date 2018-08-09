ea5cae0e925f319cd5704a2c333ab4a41455dc61
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-09 19:50:47 +0900

    no message

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public int damage = 1;
+
+    public Transform targetTransform;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Attack(PlayerHealth player)
+    {
+        player.health -= this.damage;
+        //Destroy(this.gameObject);
+    }   
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void CollidedWithEnemy(EnemyAttack enemy)
+    {
+        enemy.Attack(this);
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        EnemyAttack enemy = collision.collider.gameObject.GetComponent <EnemyAttack> ();
+        CollidedWithEnemy(enemy);
+    }
+}
