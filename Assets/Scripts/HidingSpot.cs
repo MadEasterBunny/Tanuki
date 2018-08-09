b15041094d9897f355a520ce4dfc352e22a05e12
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-09 22:31:03 +0900

    GrassHidingSpot入ったら、TagはPlayerからHidingになります。出ったら、Playerに戻ります。

---
+++ Assets/Scripts/HidingSpot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingSpot : MonoBehaviour
+{
+    GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            player.tag = "Hiding";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Hiding")
+        {
+            player.tag = "Player";
+        }
+    }
+}
