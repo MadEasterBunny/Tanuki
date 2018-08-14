13483e35a1969ae598b75227ace574ab2ed0d7b0
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-14 21:48:33 +0900

    草に入ったら、隠れるアニメーションをできるためにスクリプトを変わりました。

--- Assets/Scripts/CharacterAnimator.cs
+++ Assets/Scripts/CharacterAnimator.cs
@@ -9,12 +9,17 @@ public class CharacterAnimator : MonoBehaviour
 
     Animator animator;
     NavMeshAgent agent;
+    bool hiding;
+
+    public GameObject player;
 
 	// Use this for initialization
 	void Start ()
     {
         animator = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        //player = GetComponent<GameObject>();
+        hiding = false;
 	}
 	
 	// Update is called once per frame
@@ -22,5 +27,24 @@ public class CharacterAnimator : MonoBehaviour
     {
         float speedPercent = agent.velocity.magnitude / agent.speed;
         animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+
+        if (player.layer == 10)
+        {
+            hiding = true;
+        }
+        else
+        {
+            hiding = false;
+        }
+
+        if(hiding == true)
+        {
+            animator.SetBool("hiding", true);
+        }
+
+        if(hiding == false)
+        {
+            animator.SetBool("hiding", false);
+        }
 	}
 }
