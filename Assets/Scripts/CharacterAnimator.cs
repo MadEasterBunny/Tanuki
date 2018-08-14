670b1dd0433adf8f7b83bc8462f2bd2557210d1b
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-14 18:10:18 +0900

    Blenderから、たぬきのモデルとアニメーションをインポートしました。アニメーションのスクリプトを作りました。

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = 0.1f;
+
+    Animator animator;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+	}
+}
