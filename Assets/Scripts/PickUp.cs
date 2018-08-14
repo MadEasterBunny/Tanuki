6b1782e0ecdf3454a8885a9380234dd99c8741e8
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-14 16:44:26 +0900

    葉っぱのボタン、葉っぱや色々なものを集まるスクリプト、たぬきは葉っぱを使えるスクリプトを作りました。

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject leafButton;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            leafButton.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UseLeaf.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UseLeaf : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ChangeForms()
+    {
+        //player particle effect.
+        //deactivate tanuki graphics.
+        //activate other graphics based on random range.
+        //deactivate effects after a few seconds.
+        //play particle effect again.
+        //restore everything back to normal.
+        Debug.Log("Leaf Used.");
+    }
+}
