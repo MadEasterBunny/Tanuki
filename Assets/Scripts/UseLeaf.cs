fce4c599cdab6600cf7ca80e3c77053bd6ab252c
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-15 16:18:01 +0900

    no message

--- Assets/Scripts/UseLeaf.cs
+++ Assets/Scripts/UseLeaf.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class UseLeaf : MonoBehaviour
 {
@@ -32,7 +33,10 @@ public class UseLeaf : MonoBehaviour
     public void Tanuki()
     {
         player.GetComponent<ParticleSystem>().Play();
+        player.gameObject.layer = 0;
         player.GetComponent<PlayerController>().enabled = true;
+        player.GetComponent<PlayerMotor>().enabled = true;
+        player.GetComponent<NavMeshAgent>().enabled = true;
         player.GetComponent<CharacterAnimator>().enabled = true;
         otherGFX.SetActive(false);
         tanukiGFX.SetActive(true);
@@ -40,8 +44,11 @@ public class UseLeaf : MonoBehaviour
 
     public void OtherForm()
     {
+        player.gameObject.layer = 10;
         player.GetComponent<ParticleSystem>().Play();
         player.GetComponent<PlayerController>().enabled = false;
+        player.GetComponent<PlayerMotor>().enabled = false;
+        player.GetComponent<NavMeshAgent>().enabled = false;
         player.GetComponent<CharacterAnimator>().enabled = false;
         tanukiGFX.SetActive(false);
         otherGFX.SetActive(true);
