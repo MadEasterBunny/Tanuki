c062cd5dab0f973dfdd1b281a2745e9de41de8a0
Matthew Huntington Murphy <hutington67@aim.com>
2018-08-18 15:08:10 +0900

    隠れるスクリプトを書き直しました。
    OnTriggerEnter()はOnTriggerStay()に変わりました。

--- Assets/Scripts/HidingSpot.cs
+++ Assets/Scripts/HidingSpot.cs
@@ -17,7 +17,7 @@ public class HidingSpot : MonoBehaviour
 	
 	}
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         player.layer = 10;
     }
