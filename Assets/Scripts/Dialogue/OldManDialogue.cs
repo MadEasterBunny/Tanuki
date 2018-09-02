c606b1601d99b04a9e20608af84c2e37c5e2b625
Matthew Huntington Murphy <hutington67@aim.com>
2018-09-02 15:07:34 +0900

    敵を近く行ったら、ダイアログボックスを現れます。

---
+++ Assets/Scripts/Dialogue/OldManDialogue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class OldManDialogue : MonoBehaviour
+{
+    public Flowchart flowchart;
+    private GameObject player;
+	
+	void Start ()
+    {
+        player = PlayerManager.instance.player.gameObject;
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            flowchart.ExecuteBlock("Dialogue1");
+        }
+    }
+}
