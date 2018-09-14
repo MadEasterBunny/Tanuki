eac93b85e3c5ae029a748bb82652534843469bd3
Matthew Huntington Murphy <hutington67@aim.com>
2018-09-14 13:19:59 +0900

    スコアの表示を作りました。
    ゲームクリアした後に、盗んだ物のスコアを見えるために、スコアの表示を作りました。

--- Assets/Scripts/InsideHouse/InsideHouseCollectables/CollectableGoods.cs
+++ Assets/Scripts/InsideHouse/InsideHouseCollectables/CollectableGoods.cs
@@ -5,25 +5,22 @@ using UnityEngine.UI;
 
 public class CollectableGoods : MonoBehaviour
 {
+    public GameObject collectablesCounter;
     public Text goodsText;
-    private int stolenGoods;
+    public static int stolenGoods;
 
     //private AudioSource pickUpSound;
     public AudioClip pickUpSoundEffect;
-	
-	void Start ()
+
+    void Start ()
     {
         //pickUpSound = GetComponent<AudioSource>();
         if(goodsText != null)
         {
             goodsText.text = "盗んだ物の数：" + stolenGoods.ToString();
         }
-	}
-	
-	
-	void Update ()
-    {
-		
+
+        PlayerPrefs.GetInt("Score", 0);
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -33,6 +30,11 @@ public class CollectableGoods : MonoBehaviour
             AudioSource.PlayClipAtPoint(pickUpSoundEffect, transform.position, 0.5f);
             stolenGoods++;
             goodsText.text = "盗んだ物の数：" + stolenGoods.ToString();
+            PlayerPrefs.SetInt("Score", stolenGoods);
+            if(stolenGoods > PlayerPrefs.GetInt("Score", 0))
+            {
+                PlayerPrefs.SetInt("Score", stolenGoods);
+            }
             Destroy(this.gameObject);
         }
     }
---
+++ Assets/Scripts/OutroCutscene/OutroCutscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OutroCutscene : MonoBehaviour
+{
+    public Text resultsNumbersText;
+   
+	void Start ()
+    {
+        resultsNumbersText.text = PlayerPrefs.GetInt("Score").ToString() + "/42";
+	}
+	
+    public void EndGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
